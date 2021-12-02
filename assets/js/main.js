b69a55bb9949e839992e2154afb91fcb67cfa89f
xhasansun <xhasansun@gmail.com>
2021-12-02 20:18:53 +0300

    odev3 tamamlandi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,12 @@
 /* show menu */
-const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId),
-          nav    = document.getElementById(navId);
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav__links");
 
-    if(toggle && nav) {
-        toggle.addEventListener('click', () => {
-            nav.classList.toggle('show')
-        })
-    }
+hamburger.addEventListener("click", menu);
+
+function menu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    
 }
-
-showMenu('nav-toggle', 'nav-menu')
-
-/*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
-
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show')
-}
-
-navLink.forEach(n => n.addEventListener('click', linkAction))
 
